9b09abdf15fd9d576cdcc8ed81f885058b0ed407
markom19 <marko.mestrovic3@gmail.com>
2023-01-07 14:15:14 +0100

    new awesome config

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -347,8 +347,8 @@ clientkeys = gears.table.join(
             c:raise()
         end,
         {description = "toggle fullscreen", group = "client"}),
-    awful.key({ modkey, }, "q",      function (c) c:kill()                         end,
-              {description = "close", group = "makc"}),
+    awful.key({ modkey, "Shift" }, "c",      function (c) c:kill()                         end,
+              {description = "close", group = "client"}),
     awful.key({ modkey, "Control" }, "space",  awful.client.floating.toggle                     ,
               {description = "toggle floating", group = "client"}),
     awful.key({ modkey, "Control" }, "Return", function (c) c:swap(awful.client.getmaster()) end,
